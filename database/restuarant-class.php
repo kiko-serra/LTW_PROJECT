285b1ca38c1f235fe0695e524db9b7d982f7988e
Martim <mitram790@gmail.com>
2022-05-18 21:29:56 +0100

    Created Restaurant Class

--- database/restuarant-class.php
+++ database/restuarant-class.php
@@ -1,19 +1,66 @@
 <?php
+
 declare(strict_types=1);
 
-class Restaurant{
-    public string $name;
-    public string $title;
-    public string $description;
-    public float $reviewScore;
-    private int  $id;
+class Restaurant
+{
 
-    public function __construct($r) {
+  public string $name;
+  public string $title;
+  public string $description;
+  public float $reviewScore;
+  private int  $id;
+  private $changedList;
+
+  public function __construct($r)
+  {
     $this->name = $r["name"];
     $this->title = $r["title"];
-    $this->description= $r["category"];
+    $this->description = $r["category"];
     $this->id = intval($r["id_restaurant"]);
     $this->reviewScore = floatval($r["review_score"]);
   }
+
+
+  public function setName(string $name)
+  {
+    $this->name = $name;
+    $this->changedList["name"] = $this->name;
+  }
+  public function setTitle(string $title){
+    $this->title= $title;
+    $this->changedList["title"]= $this->title;
+  }
+  public function setDescription(string $description){
+    $this->description= $description;
+    $this->changedList["category"]= $this->description;
+  }
+  public function setReviewScore(float $reviewScore){
+    $this->reviewScore= $reviewScore;
+    $this->changedList["review_score"]= $this->reviewScore;
+  }
+
+  public function save()
+  {
+    $dbh = new PDO('sqlite:database/uber.db');
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    foreach ($this->changedList as $key => $value) {
+      $stmt = $dbh->prepare('UPDATE Restaurant SET '. $key.' = ? WHERE id_restaurant = ?');
+     
+      $stmt->execute(array($value,$this->id));
+    }
+  }
 }
+?>
+
+
+
+<?php
+// Just a temporary function to test  
+ function alterRestaurant(Restaurant $r ){
+    $r->setName("Gusteau's"); 
+    $r->setDescription("French Cuisine");
+    $r->setReviewScore(10.5);
+    $r->save();
+} 
 ?>
