9812c5526b8b24d8280502e821721c58911fe2e4
Martim <mitram790@gmail.com>
2022-05-18 18:27:54 +0100

    Created templates now index is generated in php

---
+++ templates/restaurant-tpl.php
@@ -0,0 +1,23 @@
+<?php function drawRestaurant($restaurant)
+{ ?>
+    <section class="restaurant-container">
+        <article>
+            <header>
+                <h2><?= $restaurant->name ?></h2>
+                <h3><?= $restaurant->title ?></h3>
+            </header>
+            <p><?= $restaurant->description ?></p>
+            <p><?= $restaurant->reviewScore ?></p>
+        </article>
+    </section>
+<?php } ?>
+
+
+<?php function drawRestaurants($restaurants)
+{ ?>
+    <section class="restaurants-list">
+        <?php foreach ($restaurants as $restaurant) {
+            drawRestaurant($restaurant);
+        } ?>
+    </section>
+<?php } ?>
