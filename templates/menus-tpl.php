247a4e245b7caad2dd0d5c980ff2adabae955f4f
Martim <mitram790@gmail.com>
2022-05-18 18:27:54 +0100

    Created templates now index is generated in php

---
+++ templates/menus-tpl.php
@@ -0,0 +1,26 @@
+<?php function drawFeaturedFoods()
+{  ?>
+    <section class="featured-foods">
+        <article>
+            <header>
+                <h1>First Food</h1>
+                <p>Sentence about first food topic</p>
+            </header>
+        </article>
+
+
+        <article>
+            <header>
+                <h1>Second Food</h1>
+                <p>Sentence about second food topic</p>
+            </header>
+        </article>
+
+        <article>
+            <header>
+                <h1>Third Food</h1>
+                <p>Sentence about third food topic</p>
+            </header>
+        </article>
+    </section>
+<?php } ?>
