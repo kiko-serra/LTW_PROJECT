fe9417b3fc94cb11e23ba42460ec7b38e2889e42
Martim <mitram790@gmail.com>
2022-05-30 15:59:24 +0100

    Ajax now with fetch and api folder for requests

--- templates/popup.php
+++ templates/popup.php
@@ -1,21 +1,29 @@
 <?php
 
 declare(strict_types=1);
-require_once( __DIR__ . "/../utils/session.php");
+require_once(__DIR__ . "/../utils/session.php");
 
 function drawPopUp(Session $session)
 {
-    if($session->getMessages() == null)
+    if ($session->getMessages() == null)
         return;
-      $message = $session->getMessages()[0];
-        ?>
+    $message = $session->getMessages()[0];
+?>
     <section class="pop-up-container">
-        <a class="pop-up-link" href="">X</a>
         <section class="pop-up">
-            <p><?=$message["type"]?></p>
-            <p><?=$message["text"]?></p>
+            <p><?= $message["type"] ?></p>
+            <p><?= $message["text"] ?></p>
         </section>
     </section>
+
+
+    <section id="popup">
+        <img src="/pictures/ubereats_icon.png" width="50" height="50" class="popup-img">
+        <button class="popup-close">X</button>
+            <p class="popup-text"><?= $message["type"]?></p>
+            <p class="popup-text"><?= $message["text"] ?></p>
+    </section>
+
 <?php
 }
 ?>
