87665f931a784cef7f61e551b7a49b9463d1aad1
miguelcfsilva11 <miguelcfsilva@gmail.com>
2022-06-01 12:22:20 +0100

    added waves and fixed login page

--- templates/auth-tpl.php
+++ templates/auth-tpl.php
@@ -7,6 +7,22 @@ declare(strict_types=1);
 {
 
 ?>
+
+    <section>
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </section>
+
     <section class="signup-page">
         <header>
             <h2>Sign Up</h2>
@@ -44,6 +60,8 @@ declare(strict_types=1);
             <p>Already have an account? <a href="../pages/login.php"><span style="font-weight: 500">Login</spam></a></p>
         </footer>
     </section>
+
+
     <?php drawPopUp($session); ?>
     </body>
 
@@ -64,6 +82,7 @@ declare(strict_types=1);
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Eats</title>
         <link rel="stylesheet" href="../css/style.css">
+        <link rel = "stylesheet" href = "../css/waves.css">
         <link href='//fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <script src="../javascript/restaurantAjax.js" defer></script>
@@ -72,31 +91,51 @@ declare(strict_types=1);
     </head>
 
     <body>
+        
 
         <section class="login-page">
-            <form class="login-form" action="../actions/action_login.php" method="post">
+            
 
-                <h2>Uber Eats</h2>
-                <h3>Login</h3>
-                <input type="text" name="username">
-                <input type="text" name="password">
-                <input type="submit" value="Submit">
-                <a href="./signup.php"><button type="button">Register</button></a>
+            <section class = "waves-login">
+                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <g class="parallax">
+                        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </g>
+                </svg>
+            </section>
+                <section class ="right-login-background">
+                <form class="login-form" action="../actions/action_login.php" method="post">
 
-                <footer class="login-footer">
-                    <ul>
-                        <li>Local</li>
-                        <li><a href="https://www.uber.com/legal/en/document/?name=general-terms-of-use&country=portugal&lang=en">Copyright</a>
-                        </li>
-                        <li>Owners</li>
-                        <li>Careers</li>
+                
+                    <h2>Uber Eats</h2>
+                    <h3>Login</h3>
+                    <input type="text" name="username">
+                    <input type="text" name="password">
+                    <input type="submit" value="Submit">
+                    <a href="./signup.php"><button type="button">Register</button></a>
 
-                    </ul>
-                </footer>
-            </form>
+                    <footer class="login-footer">
+                        <ul>
+                            <li>Local</li>
+                            <li><a href="https://www.uber.com/legal/en/document/?name=general-terms-of-use&country=portugal&lang=en">Copyright</a>
+                            </li>
+                            <li>Owners</li>
+                            <li>Careers</li>
 
+                        </ul>
+                    </footer>
+                </form>
 
-            <section class="login-background">
+                </section>
+
+            <section class="left-login-background">
                 <a href="../index.php"><img src="../docs/images/ubereats_logo.jpg"></a>
             </section>
 
