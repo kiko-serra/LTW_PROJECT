c878c9c2f1fccd47f06b9b7e490f79380fa20826
miguelcfsilva11 <miguelcfsilva@gmail.com>
2022-06-02 22:47:53 +0100

    Fixed sideScroller values

--- javascript/sideScroller.js
+++ javascript/sideScroller.js
@@ -2,9 +2,9 @@ const buttonLeft = document.querySelector('.scroll-left');
 const buttonRight = document.querySelector('.scroll-right');
 
 buttonRight.onclick = function () {
-    document.querySelector('.scrolling-wrapper').scrollLeft += 400;
+    document.querySelector('.scrolling-wrapper').scrollLeft += 700;
 };
 
 buttonLeft.onclick = function () {
-    document.querySelector('.scrolling-wrapper').scrollLeft -= 400;
+    document.querySelector('.scrolling-wrapper').scrollLeft -= 700;
 };
