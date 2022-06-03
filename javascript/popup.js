2536710d85f34ba907fab05e600d0d1fc5eeba73
Martim <mitram790@gmail.com>
2022-06-03 01:05:08 +0100

    Rebase nani

--- javascript/popup.js
+++ javascript/popup.js
@@ -5,3 +5,7 @@ const closeButton = popUp.querySelector("button.popup-close")
 closeButton.addEventListener("click",()=>{
     popUp.style.display = "none"
 })
+// Fade Pop up
+setTimeout(() =>{
+    popUp.style.display = "none"
+},3* 1000)
