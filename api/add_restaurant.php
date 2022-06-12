3d1463f711b873fe289671e98593bc664f3c1fde
Martim <mitram790@gmail.com>
2022-06-12 19:21:15 +0100

    Refactored action add restaurant into an api

---
+++ api/add_restaurant.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+$params = ["name","address","category","reviewScore","title","p_id"];
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+foreach ($params  as $p) {
+  if (!isset($_POST[$p]))
+    die(json_encode("Missing Parameters cao " . $p ));
+}
+
+
+require_once(__DIR__ . '/../utils/session.php');
+$session = new Session();
+
+require_once(__DIR__ . '/../database/connection.php');
+require_once(__DIR__ . '/../database/restaurant-class.php');
+try{
+$db = getDatabaseConnection();
+
+$name = $_POST['name'];
+$address = $_POST['address'];
+$category = $_POST['category'];
+$reviewScore = $_POST['reviewScore'];
+$title = $_POST['title'];
+
+// Don't allow certain characters
+if (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
+  die(json_encode(array("name" => 'Name for Restaurant can only contain letters and numbers!')));
+}
+$restaurant = Restaurant::insertRestaurant($db, $session->getId(), $name, $title, $category, $reviewScore, $address);
+/* Discover How to do this in js
+ $session->addMessage('success', 'Restaurant added!');
+$next = '../pages/restaurant-page.php?id=' . $restaurant->id . '&name=' . $restaurant->name;
+header('Location: ' . $next);
+*/
+$next = '../pages/restaurant-page.php?id=' . $restaurant->id . '&name=' . $restaurant->name;
+die(json_encode(array("restaurant" =>$next)));
+}catch(Exception $e){
+  die(json_encode("Error while accessing db"));
+}
+?>
