8e71816bf0ce6c74b4822b339f63471d78507ae4
Martim <mitram790@gmail.com>
2022-06-13 22:57:19 +0100

    Toggle Favourite Menu endpoint that when passed id_user and id_menu it toggles from unfavourite to favourite

---
+++ api/toggle_favourite_menu.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(__DIR__ . "/../database/connection.php");
+$params = ["id_menu", "id_user"];
+$_POST= json_decode(file_get_contents('php://input'),true);
+
+foreach($params  as $p){
+    if(!isset($_POST[$p]))
+        die(json_encode("Missing Parameters"));
+} 
+
+$id_menu = $_POST["id_menu"];
+$id_user = $_POST["id_user"];
+
+try {
+    $db = getDatabaseConnection();
+
+    $stmt = $db->prepare("select * from FavouriteMenu where id_user = ? and id_menu=?");
+    $stmt->execute(array($id_user, $id_menu));
+    $favourite = $stmt->fetch();
+    if ($favourite) {
+        $stmt = $db->prepare("Delete from  FavouriteMenu where id_user=? and id_menu=?");
+        $stmt->execute(array($id_user, $id_menu));
+        die(json_encode("Successefully removed menu from favourites"));
+    } else {
+        $stmt = $db->prepare("Insert into FavouriteMenu (id_user,id_menu) values (?,?)");
+        $stmt->execute(array($id_user, $id_menu));
+        die(json_encode("Successefully added menu to favourites"));
+    }
+} catch (Exception $e) {
+    die(json_encode("Error while acessing db :" . $e));
+}
