25beff7260229eeaf4d5697f6ef2a73f4e29be22
Martim <mitram790@gmail.com>
2022-06-13 23:27:42 +0100

    Added functionality to the favourite buttons -> ajax request to api toggle_favourite_menu

---
+++ javascript/favouriteMenu.js
@@ -0,0 +1,20 @@
+
+
+const menuCards = document.querySelectorAll("section.menu-container")
+const id_user = document.querySelector("#user-icon").getAttribute("id")
+for (const card of menuCards) {
+    const favouriteButton = card.querySelector("button.favourite-menu")
+    const id_menu = card.getAttribute("id_menu")
+    favouriteButton.addEventListener("click", async () => {
+        const data = { "id_user": id_user, "id_menu": id_menu }
+        const response = await fetch("../api/toggle_favourite_menu.php",
+            {
+                method: "POST",
+                body: JSON.stringify(data)
+            }
+        )
+        const result = await response.json()
+        console.log(result)
+
+    })
+}
