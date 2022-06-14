c6eb7711a1d6d74926e9dd1ba6d6d0824386a943
Martim <mitram790@gmail.com>
2022-06-14 17:25:03 +0100

    Created api to add comment

---
+++ api/add_comment.php
@@ -0,0 +1,29 @@
+<?php
+require_once(__DIR__ ."/../database/connection.php");
+$params = ["id_restaurant","title","comment"];
+$_POST= json_decode(file_get_contents('php://input'),true);
+
+foreach($params  as $p){
+    if(!isset($_POST[$p]))
+        die(json_encode("Missing Parameters"));
+}
+$id_restaurant = $_POST["id_restaurant"];
+$title = $_POST["title"];
+$comment = $_POST["comment"];
+
+$session = new Session();
+
+// Needs to be filtered
+try{
+
+    $db = getDatabaseConnection();
+    $stmt = $db->prepare("insert into comment (id_user,id_restaurant,comment,title) values (?,?,?,?)");
+    $stmt->execute(array($session->getId(),$id_restaurant,$comment,$title));
+
+    
+
+}catch(Exception $e){
+    die(json_encode("Database Error:" . $e ));
+}
+
+?>
