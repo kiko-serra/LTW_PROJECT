b3b30fe722a286d08aa00ba6773544b2c81c6ac9
Martim <mitram790@gmail.com>
2022-06-14 18:28:01 +0100

    Api add comment now returns the inserted comment

--- api/add_comment.php
+++ api/add_comment.php
@@ -21,7 +21,12 @@ try{
     $db = getDatabaseConnection();
     $stmt = $db->prepare("insert into comment (id_user,id_restaurant,comment,title) values (?,?,?,?)");
     $stmt->execute(array($session->getId(),$id_restaurant,$comment,$title));
-    die(json_encode("Successefully added comment"));
+    $id_comment = $db->lastInsertId();
+    $stmt = $db->prepare("Select username from User where id_user =?");
+    $stmt->execute(array($session->getId()));
+    $username = $stmt->fetch()["username"];
+    die(json_encode(array("id"=>$id_comment,"id_restaurant"=>$id_restaurant,
+    "comment"=>$comment , "title" =>$title,"username"=>$username)));
 
 }catch(Exception $e){
     die(json_encode("Database Error:" . $e ));
