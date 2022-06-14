a1c5d65f1de541ce7436f961970ccdff3472ffb1
Martim <mitram790@gmail.com>
2022-06-14 18:38:49 +0100

    After comenting commenet is automatically inserted in the comments list

--- javascript/addComment.js
+++ javascript/addComment.js
@@ -1,4 +1,19 @@
 
+const buildComment = (comment) => {
+    return `
+    <section class="restaurant-comment" id=${comment.id}>
+           <h2 class="comment-title">${comment.title}</h2>
+        <p class="comment-text">
+            <span class="comment-username">${comment.username}</span>
+            ${comment.comment}
+        </p>
+    </section>`
+}
+
+const addComment = (htmlComment) => {
+    const commentsList = document.querySelector("section.restaurant-comments")
+    commentsList.innerHTML += htmlComment
+}
 
 const addCommentClick = () => {
     const addCommentButton = document.querySelector("button.add-comment")
@@ -7,13 +22,15 @@ const addCommentClick = () => {
         const restaurantId = commentCard.getAttribute("id_restaurant")
         const title = commentCard.querySelector("input[name=title]").value
         const comment = commentCard.querySelector("input[name=comment]").value
-
+        commentCard.querySelector("input[name=title]").value = ""
+        commentCard.querySelector("input[name=comment]").value = ""
         const data = { "id_restaurant": restaurantId, "title": title, "comment": comment }
         const response = await fetch("../api/add_comment.php", {
             method: "POST",
             body: JSON.stringify(data)
         })
-        console.log(await response.json())
+        const commentResult = await response.json()
+        addComment(buildComment(commentResult))
     })
 
 }
