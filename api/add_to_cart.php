3e443352258f7d39c1743dd691406ab6ad5260ee
Martim <mitram790@gmail.com>
2022-06-14 15:10:56 +0100

    Add to cart now doesn't receive id user through post

--- api/add_to_cart.php
+++ api/add_to_cart.php
@@ -9,14 +9,9 @@ foreach($params  as $p){
         die(json_encode("Missing Parameters"));
 }
 
-$id_user = $_POST["id_user"];
 $id_menu = $_POST["id_menu"];
 
 $session = new Session();
-
-if($id_user !== $session->getId()){
-    die(json_encode("Something went really wrong"));
-}
 $session->addOrder(intval($id_menu));
 die(json_encode("Added Menu to order"));
 
